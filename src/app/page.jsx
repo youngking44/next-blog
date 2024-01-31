5a21b6fb097dbfa35e11c07ed1c8ca06235c717c
Mike King <youngking4406@gmail.com>
2024-01-31 11:35:16 -0800

    Updated Hero

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,10 +1,27 @@
 import Image from "next/image";
-import Hero from "public/heroImage.png";
+import styles from "./page.module.css";
 
 export default function Home() {
   return (
     <div>
-      <Image src={Hero} alt="e" />
+      <div className={styles.item}>
+        <h1 className={styles.title}>
+          Better design for your digital products.
+        </h1>
+        <p className={styles.desc}>
+          Turning your idea into reality . We bring together the team from
+          global tech industry.
+        </p>
+        <button className={styles.title}>See our works.</button>
+      </div>
+      <div className={styles.item}>
+        <Image
+          fill={true}
+          src="/hero.png"
+          className={styles.img}
+          alt="Hero image"
+        />
+      </div>
     </div>
   );
 }
