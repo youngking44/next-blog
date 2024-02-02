abf2db5b1ccbb982e0e51a23924634cc74ba99e8
Youngking Mike <youngking4406@gmail.com>
2024-02-02 11:37:59 +0000

    Updated

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -1,34 +1,17 @@
 "use client";
 
-import React, { useEffect, useState } from "react";
+import React from "react";
+import useSWR from "swr";
 
 const Dashboard = () => {
-  const [data, setData] = useState([]);
-  const [err, setErr] = useState(false);
-  const [loading, setLoading] = useState(false);
+  const fetcher = (...args) => fetch(...args).then((res) => res.json());
 
-  useEffect(() => {
-    const getData = async () => {
-      setLoading(true);
+  const { data, error, isLoading } = useSWR(
+    "https://jsonplaceholder.typicode.com/posts",
+    fetcher
+  );
 
-      const res = await fetch(`https://jsonplaceholder.typicode.com/posts`, {
-        cache: "no-store",
-      });
-
-      if (!res.ok) {
-        setErr(true);
-      }
-
-      const resData = await res.json();
-
-      setLoading(false);
-      setData(resData);
-    };
-
-    getData();
-  }, []);
-
-  console.log("Data...", data);
+  console.log("My swr data...", data);
 
   return <div>Dashboard</div>;
 };
